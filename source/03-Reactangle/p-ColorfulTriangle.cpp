1ce90929c740e3f027174613f720f727a5f76f0b
wangrui <wangrui@weidian.com>
2023-05-31 14:29:15 +0800

    feat: 实践证明，使用两个 shaderProgram 可以同时绘制不同颜色的三角形

--- source/03-Reactangle/p-ColorfulTriangle.cpp
+++ source/03-Reactangle/p-ColorfulTriangle.cpp
@@ -48,7 +48,7 @@ int main()
         return -1;
     }
 
-    unsigned int fragmentShader = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
 
@@ -74,8 +74,35 @@ int main()
         return -1;
     }
 
+unsigned int fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
+    glCompileShader(fragmentShader2);
+
+    glGetShaderiv(fragmentShader2, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader2, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED 2222\n"
+                  << infoLog << std::endl;
+        return -1;
+    }
+
+    unsigned int shaderProgram2 = glCreateProgram();
+    glAttachShader(shaderProgram2, vertexShader);
+    glAttachShader(shaderProgram2, fragmentShader2);
+    glLinkProgram(shaderProgram2);
+    glGetProgramiv(shaderProgram2, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram2, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED 222\n"
+                  << infoLog << std::endl;
+        return -1;
+    }
+
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+    glDeleteShader(fragmentShader2);
 
     float vertices1[] = {
         0.0f, -0.5f, 0.0f, // left
@@ -96,6 +123,8 @@ int main()
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO1);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
 
     unsigned int VBO2, VAO2;
     glGenVertexArrays(1, &VAO2);
@@ -139,6 +168,8 @@ int main()
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO1);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glUseProgram(shaderProgram2);
 
         glBindVertexArray(VAO2);
         glDrawArrays(GL_TRIANGLES, 0, 3);
